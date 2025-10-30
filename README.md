0621a26d20cfca38dfa27495d088406b9603dbcb
Bryan Alvarado <bryanalvarado1904@gmail.com>
2025-10-29 19:36:13 -0600

    Agregar archivos iniciales para la gestión de héroes, incluyendo HTML, CSS, JavaScript y datos JSON.

---
+++ README.md
@@ -0,0 +1,13 @@
+tratamiento de array en javascript
+Al trabajar con base de datos, se trabaja con sql --> data --> array
+
+-> funcion map
+-> forEach
+-> for of
+-> for in
+-> filter
+-> reduce
+-> groupBy
+
+-> arreglos de objetos
+[{}]
---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+// liga para el formulario de superheroes
+const form = document.getElementById('form-heroes')
+const container = document.getElementById('container-heroes')
+
+//escuchar el evento submit del formulario
+form.addEventListener('submit', async (event) => {
+  //TODO: evitar que se envie al formulario
+  event.preventDefault()
+
+  //TODO: Obtener los datos del json de superheroes
+  const url = `/static/data/heroes.json`
+  const heroes = await fetch(url).then((Response) => Response.json())
+
+  heroes.forEach((hero) => {})
+})
